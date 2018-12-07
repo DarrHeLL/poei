0d244bd828f22ce4dd2ac44ae2639f4913a3a851
DarrHeLL <darrhell95@gmail.com>
2018-12-07 14:10:45 +0100

    Ajout de jquery_ui_tabs

---
+++ web/themes/custom/ive/layouts/jquery_tabs/jquery-ui-tabs.js
@@ -0,0 +1,8 @@
+/**
+ * Created by POE1 on 07/12/2018.
+ */
+(function ($, Drupal, DrupalSettings) {
+    $(document).ready(function () {
+        $('#jquery-tabs').tabs();
+    });
+})(jQuery, Drupal, drupalSettings);
